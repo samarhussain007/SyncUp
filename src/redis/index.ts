778b500eacd82cd02bb7073b63cede833453d215
samarhussain007 <hussainsamar36@gmail.com>
2024-12-29 18:53:27 +0530

    created the realtime layer for syncing data between the host and the user

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,11 +1,17 @@
 import Redis from "ioredis";
 import { redisConfigs } from "../config";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
 console.log(redisConfigs.redisHost);
 
 const redis = new Redis({
   host: redisConfigs.redisHost,
   port: Number(redisConfigs.redisPort),
+});
+
+export const pubsub = new RedisPubSub({
+  publisher: new Redis(Number(redisConfigs.redisPort), redisConfigs.redisHost), // Dedicated publisher client
+  subscriber: new Redis(Number(redisConfigs.redisPort), redisConfigs.redisHost),
 });
 
 export default redis;
--- src/resolvers/LobbyGeneration.ts
+++ src/resolvers/LobbyGeneration.ts
@@ -1,9 +1,12 @@
+import { subscribe } from "diagnostics_channel";
 import {
   createJam,
   CreateJamInputs,
   getAllAvailableJams,
   getJamByJamCode,
 } from "../services/lobbyGeneration";
+import { pubsub } from "../redis";
+const JAM_UPDATE_EVENT = "JAM_UPDATE_EVENT";
 
 const LobbyGenerationResolvers = {
   Query: {
@@ -37,8 +40,43 @@ const LobbyGenerationResolvers = {
       console.log(result);
       return result;
     },
+    updateJam: async (
+      _: any,
+      {
+        jamCode,
+        newStatus,
+      }: {
+        jamCode: string;
+        newStatus: string;
+      }
+    ) => {
+      // Simulating an update to the jam. In a real application, you'd update your database here.
+      const updatedJam = {
+        jamCode,
+        status: newStatus, // new status for the jam
+      };
+
+      // Publish to the corresponding jamCode channel
+      pubsub.publish(`${JAM_UPDATE_EVENT}:${jamCode}`, {
+        onJamUpdate: updatedJam,
+      });
+
+      // Return the updated jam object
+      return updatedJam;
+    },
   },
-  //   Subscription: {},
+  Subscription: {
+    onJamUpdate: {
+      subscribe: (root: any, args: { jamCode: string }, context: any) => {
+        const { jamCode } = args;
+
+        // Directly subscribe to the jam update event based on the jamCode
+        console.log("Listening to updates for jamCode:", jamCode);
+
+        return pubsub.asyncIterator(`${JAM_UPDATE_EVENT}:${jamCode}`);
+      },
+    },
+  },
 };
 
 export default LobbyGenerationResolvers;
